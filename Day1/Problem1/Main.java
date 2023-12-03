571282a3e51536ec6438c88459af9a96650df32c
Kyle Neil <kyleneil1403@gmail.com>
2023-12-03 01:07:59 +0000

    #2 Problem 1 complete

--- Day1/Problem1/Main.java
+++ Day1/Problem1/Main.java
@@ -1,5 +1,44 @@
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int total = 0;
+        try{
+            FileReader fr = new FileReader("./input.txt");
+            Scanner sc = new Scanner(fr);
+            
+            while(sc.hasNextLine()){
+            String line = sc.nextLine();
+            total += extractNumberFromString(2,line);
+            }
+
+            sc.close();
+            fr.close();
+        }
+        catch(Exception e){
+            System.out.println(e);
+            System.exit(1);
+        }
+        finally{
+            System.out.println(total);
+        }
+        
+    }
+
+    protected static int extractNumberFromString(int digits, String string){
+        string = string.replaceAll("[^\\d]","");
+        //System.out.println(string);
+        String finalNumber = new String();
+        if(string.length()==1){
+            finalNumber += string.charAt(0);
+            finalNumber += string.charAt(0);
+        }
+        else{
+            finalNumber += string.charAt(0);
+            finalNumber += string.charAt(string.length()-1);
+        }
+        
+        return Integer.parseInt(finalNumber);
     }
 }
